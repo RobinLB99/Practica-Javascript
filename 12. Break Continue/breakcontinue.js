82a9658be242f310984ff90bcca41684a8f22c2a
RobinLB99 <97130422+RobinLB99@users.noreply.github.com>
2022-01-04 21:56:18 -0500

    Comentario

--- 12. Break Continue/breakcontinue.js
+++ 12. Break Continue/breakcontinue.js
@@ -1,5 +1,6 @@
 let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 
+/* Break rompera la extructura de contro. En el caso de este ciclo, al llegar a la posicion 5 del arreglo bloque dejara todo el ciclo dejara de ejecutarse. */
 for (var i = 0; i < numeros.length; i++){
     if (i === 5){
         break;
@@ -7,6 +8,7 @@ for (var i = 0; i < numeros.length; i++){
     console.log(numeros[i]);
 }
 
+/* Continue. En el caso de ciclo, al llegar a la posicion 5 omitira la ejecucion del bloque en esa iteracion del ciclo y continuara a la siguiente. */
 for (var i = 0; i < numeros.length; i){
     if (i === 4){
         continue;
