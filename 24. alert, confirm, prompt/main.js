04479ad1489d98de1288071011b875371d1fe836
RobinLB99 <97130422+RobinLB99@users.noreply.github.com>
2022-02-20 13:59:05 -0500

    alert, confirm, prompt

---
+++ 24. alert, confirm, prompt/main.js
@@ -0,0 +1,13 @@
+console.log(window);
+/*
+alert('Esto es una alerta');
+confirm('Esto es una confirmacion');
+prompt('Esto es un prompt y pido datos al usuario');
+*/
+let alerta = alert('Esto es una alerta'),
+    confirmacion = confirm('Esto es una confirmacion'),
+    aviso = prompt('Esto es un prompt y pido datos al usuario');
+
+console.log(alerta);
+console.log(confirmacion);
+console.log(aviso);
