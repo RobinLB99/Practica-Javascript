eb5c30bd0faf76e93b146109a67a75576ab1dab6
RobinLB99 <97130422+RobinLB99@users.noreply.github.com>
2022-03-18 21:36:40 -0500

    Ejercicios de logica de programacion 5

--- 28. Ejercicios de logica de programacion/ejercicios5.js
+++ 28. Ejercicios de logica de programacion/ejercicios5.js
@@ -1,4 +1,5 @@
-/* 15) Programa una función para convertir números de base binaria a decimal y viceversa, pe. miFuncion(100,2) devolverá 4 base 10. */
+/* 15) Programa una función para convertir números de base binaria a decimal y viceversa,
+pe. miFuncion(100,2) devolverá 4 base 10. */
 
 const convertirBase = (numero = undefined, base = undefined) => {
     if(numero === undefined) return console.warn(`No ingreso el numero a convertir`);
@@ -48,7 +49,8 @@ const convertirBase = (numero = undefined, base = undefined) => {
 
 convertirBase(101 , 2)
 
-/* 16) Programa una función que devuelva el monto final después de aplicar un descuento a una cantidad dada, pe. miFuncion(1000, 20) devolverá 800. */
+/* 16) Programa una función que devuelva el monto final después de aplicar un descuento a una cantidad
+dada, pe. miFuncion(1000, 20) devolverá 800. */
 
 const descuento = (valor = undefined, porcentRebaja = undefined) => {
     if (valor === undefined) return console.warn(`No ingreso un valor`);
@@ -65,7 +67,8 @@ const descuento = (valor = undefined, porcentRebaja = undefined) => {
 descuento(1000, 20);
 
 
-/* 17) Programa una función que dada una fecha válida determine cuantos años han pasado hasta el día de hoy, pe. miFuncion(new Date(1984,4,23)) devolverá 35 años (en 2020). */
+/* 17) Programa una función que dada una fecha válida determine cuantos años han pasado hasta el día
+de hoy, pe. miFuncion(new Date(1984,4,23)) devolverá 35 años (en 2020). */
 
 const años = (fecha = undefined) => {
     if (fecha === undefined) return console.warn(`No ingreso la fecha`);
