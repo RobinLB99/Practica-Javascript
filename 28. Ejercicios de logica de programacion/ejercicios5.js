9f606093b4bd5b8b09f3f779b7cd5e5a568cef94
RobinLB99 <97130422+RobinLB99@users.noreply.github.com>
2022-03-18 21:26:37 -0500

    Ejercicios de logica de programacion 5

--- 28. Ejercicios de logica de programacion/ejercicios5.js
+++ 28. Ejercicios de logica de programacion/ejercicios5.js
@@ -1,7 +1,52 @@
 /* 15) Programa una función para convertir números de base binaria a decimal y viceversa, pe. miFuncion(100,2) devolverá 4 base 10. */
 
+const convertirBase = (numero = undefined, base = undefined) => {
+    if(numero === undefined) return console.warn(`No ingreso el numero a convertir`);
+    if(typeof numero !== "number") return console.error(`El valor a convertir debe ser tipo numerico`);
+    if(base === undefined) return console.warn(`No ingreso la base del numero a convertir`);
+    if(typeof base !== "number") return console.error(`El valor de base debe ser tipo numerico`);
+    if (base !== 10 && base !== 2) return console.error(`La base ${base} no es una base valida`);
 
+    if(base === 10) {
+        i = numero;
+        binario = [];
+        while(i > 0){
+            let base2 = i % 2;
+            binario.push(base2);
+            i = Math.floor(i / 2);
+        }
+        binario = binario.reverse().join("");
+        return console.info(`${numero} base ${base}  =  ${binario} base 2`);
+    }
+    if(base === 2) {
+        let base2 = numero.toString().split("");
 
+        expReg = /[2-9]/.test(base2);
+
+        if(expReg === false){
+            decimal = [];
+
+            for (let i = 0; i < base2.length; i++) {
+                exp = 2;
+                digito = base2[i] * Math.pow(2, exp);
+                decimal.push(digito);
+                exp--;
+            }
+
+            let base10 = 0;
+
+            for(let i = 0; i < decimal.length; i++) {
+                base10 = base10 + decimal[i];
+            }
+            return console.info(`${numero} base ${base}  =  ${base10} base 10`);
+        }
+        else{
+            console.error(`${numero} no es de base 2`);
+        }
+    }
+}
+
+convertirBase(101 , 2)
 
 /* 16) Programa una función que devuelva el monto final después de aplicar un descuento a una cantidad dada, pe. miFuncion(1000, 20) devolverá 800. */
 
