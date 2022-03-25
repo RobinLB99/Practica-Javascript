c3b39a14cfe16f3a3e8480be5c9293f40031f4a3
RobinLB99 <97130422+RobinLB99@users.noreply.github.com>
2022-03-25 17:19:53 -0500

    Expresiones regulares

--- 25. Expresion regulares/main.js
+++ 25. Expresion regulares/main.js
@@ -2,20 +2,20 @@
     Son una secuencia de caracteres que forman un patron de busqueda, principalmente
     utilizada para la busqueda de patrones de cadenas de caracteres */
 
-let cadena = `Cadena de texto 1 en javascript. Es4to es una cadena de texto que yo mismo he hecho para poner ejemplo en est2a practica de Javascript. 1232`;
+let cadena = "Cadena de texto 1 en javascript. Es4to es una cadena de texto que yo mismo he hecho para poner ejemplo en est2a practica de Javascript. 1232";
 
 let expReg = new RegExp("javascript","i");
     /* La bandera i ignora las mayusculas y minusculas */
 
-let expReg2 = new RegExp("javascript","ig");
+let expReg2 = new RegExp("javascript","gi");
     /* La bandera g no solo se queda con primera coincidencia sino con todas */
 
-let expReg3 = /javascript/ig;
+let expReg3 = /javascript/gi;
 
-let expReg4 = /\d/ig // \d - busca un numero
-let expReg5 = /[0-9]/ig // \[] - busca un numero en un rango determinado
-let expReg6 = /javascript{1}/ig;//al menos la palabra javascript se tiene que repetir 1 vez.
-let expReg7 = /javascript{3}/ig;//al menos la palabra javascript se tiene que repetir 3 veces.
+let expReg4 = /\d/gi // \d - busca un numero
+let expReg5 = /[0-9]/gi // \[] - busca un numero en un rango determinado
+let expReg6 = /javascript{1}/gi;//al menos la palabra javascript se tiene que repetir 1 vez.
+let expReg7 = /javascript{3}/gi;//al menos la palabra javascript se tiene que repetir 3 veces.
 /* Comprueba que la exprecion exista en la variable. */
     console.log(expReg.test(cadena)); // true
 
